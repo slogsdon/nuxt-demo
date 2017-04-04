401f3821c7bd40338dd46db0c0bc7a6cb945395b
Shane Logsdon <shane@shanelogsdon.com>
2017-04-04 01:19:33 -0400

    remove unnecessary files

--- docs/_nuxt/3.nuxt.bundle.ac7879c8b33581207ace.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([3],{62:function(n,e,t){var r=t(2)(null,t(70),null,null);n.exports=r.exports},70:function(n,e){n.exports={render:function(){var n=this,e=n.$createElement;return(n._self._c||e)("nuxt")},staticRenderFns:[]}}});
